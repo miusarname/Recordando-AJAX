b7994a6c9aa6c5d41a94f863934a7564f4a5c3c9
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2022-12-30 11:02:15 -0500

    Recordé AJAX XMLHttpRequest

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Aprendiendo-AJAX
- Aprendiendo y/o recordando AJAX para un proxima aplicacion
+# Recordando-AJAX
+ Recordando AJAX para un proxima aplicacion
+# Nota
+  Ya conocia AJAX, su funcionamiento y su uso sin embargo olvide como usarla y la necesito para un proximo proyecto que el dia de hoy he de subir
+    Att:Oscar M Alvarez G
---
+++ Util/ajax.js
@@ -0,0 +1,36 @@
+(() => {
+  const xhr = new XMLHttpRequest(),
+    $xhr = document.getElementById("xhr");
+  $fragment = document.createDocumentFragment();
+
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+    console.log(xhr);
+    if (xhr.status >= 200 && xhr.status < 300) {
+      console.log("exito");
+      //console.log(xhr.responseText);
+      let json = JSON.parse(xhr.responseText);
+      console.log(json);
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $xhr.appendChild($fragment)
+    } else {
+      
+      console.log("error");
+      let message =xhr.statusText || "ocurrio un error"
+      $xhr.innerHTML= `Error.${xhr.status}:${message}`
+    }
+
+    console.log("este msj cargara");
+  });
+
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/user");
+
+  xhr.send();
+})();
